fde7241c0a403ee04ce48f4940c35453ecf6ddde
Ignacio Consuegra <ignacioandresconsuegradelacruz@gmail.com>
2025-10-22 16:32:42 -0500

    create user, login and logout works fine.

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -39,9 +39,35 @@ export const signup = async (req, res) => {
     res.status(500).json({ message: "Internal server Error" });
   }
 };
-export const login = (req, res) => {
-  res.send("login Route");
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+    generateToken(user._id, res);
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Internal server Error" });
+  }
 };
 export const logout = (req, res) => {
-  res.send("logout Route");
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (err) {
+    res.status(500).json({ message: "Internal server Error" });
+  }
 };
+
+export const updateProfile = async (req, res) => {};
