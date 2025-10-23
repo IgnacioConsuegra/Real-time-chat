1c4af6efdde7f5dbe7c0705f0e328e1a9f0ec6e7
Ignacio Consuegra <ignacioandresconsuegradelacruz@gmail.com>
2025-10-23 08:45:03 -0500

    create the routes for the messages.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,10 @@
 import express from "express";
-import authRoutes from "../routes/auth.route.js";
 import dotenv from "dotenv";
-import { connectDB } from "../lib/db.js";
 import cookieParser from "cookie-parser";
-import messageRoutes from "./routes/message.route.js";
+import authRoutes from "../routes/auth.route.js";
+import messageRoutes from "../routes/message.route.js";
+import { connectDB } from "../lib/db.js";
+
 const app = express();
 
 dotenv.config();
