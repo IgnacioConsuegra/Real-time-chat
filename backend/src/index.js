ad348525d06455b3d929d06aed6c9e238adf71ca
Ignacio Consuegra <ignacioandresconsuegradelacruz@gmail.com>
2025-10-23 07:48:19 -0500

    singup, login, check and logout routes works fine.

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import authRoutes from "../routes/auth.route.js";
 import dotenv from "dotenv";
 import { connectDB } from "../lib/db.js";
+import cookieParser from "cookie-parser";
+import messageRoutes from "./routes/message.route.js";
 const app = express();
 
 dotenv.config();
@@ -10,11 +12,9 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
-
-app.get("/", (req, res) => {
-  res.send("HI there");
-});
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("Listening on port:", PORT);
