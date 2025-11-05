172a8ddab5f02e02f3e6820ef507e79a2dce6cdf
Ignacio Consuegra <ignacioandresconsuegradelacruz@gmail.com>
2025-11-05 16:26:08 -0500

    Project completed.

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,12 +6,12 @@ import messageRoutes from "../routes/message.route.js";
 import { connectDB } from "../lib/db.js";
 import { app, server } from "../lib/socket.js";
 import cors from "cors";
-
+import path from "path";
 dotenv.config();
 
 // Use PORT from environment, fallback to 5000
 const PORT = process.env.PORT || 5000;
-
+const __dirname = path.resolve();
 app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ limit: "2mb", extended: true }));
 app.use(cookieParser());
@@ -25,6 +25,12 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "./fronted/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../fronted", "dist", "index.html"));
+  });
+}
 server.listen(PORT, () => {
   console.log("Listening on port:", PORT);
 
