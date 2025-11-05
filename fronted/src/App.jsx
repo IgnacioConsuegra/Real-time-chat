03cd179d871629d6f427787ba6beb63df08ebd7f
Ignacio Consuegra <ignacioandresconsuegradelacruz@gmail.com>
2025-11-05 16:43:02 -0500

    Project completed

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -15,7 +15,6 @@ import { useThemeStore } from "./store/useThemeStore";
 const App = () => {
   const {authUser, checkAuth, isCheckingAuth, onlineUsers} =  useAuthStore();
   const {theme} = useThemeStore();
-  console.log({onlineUsers})
   useEffect(() => {
     checkAuth()
   }, [checkAuth]);
